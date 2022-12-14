445119fbfe4567cfd8c2926bf2dc55c49c0c9255
deiz0n <eduardomacedosilva@outlook.com>
2022-12-14 15:34:36 -0300

    Criação da classe Departamento

---
+++ src/aplicacao/Programa.java
@@ -0,0 +1,15 @@
+package aplicacao;
+
+import entidades.Departamento;
+
+public class Programa {
+
+    public static void main(String[] args) {
+
+        Departamento departamento = new Departamento(1, "Books");
+
+        System.out.println(departamento);
+
+    }
+
+}
---
+++ src/entidades/Departamento.java
@@ -0,0 +1,54 @@
+package entidades;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Departamento implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Integer id;
+    private String nome;
+
+    public Departamento(Integer id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getnome() {
+        return nome;
+    }
+
+    public void setnome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Departamento that = (Departamento) o;
+        return id.equals(that.id) && Objects.equals(nome, that.nome);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nome);
+    }
+
+    @Override
+    public String toString() {
+        return "Departamento{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                '}';
+    }
+}
